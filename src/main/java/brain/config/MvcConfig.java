d931a0908cdf1cb2fd8f65fde2d4e1f68014df5a
SeeSoon21 <egornesobaka@yandex.ru>
2022-11-29 21:39:07 +0300

    another one commit before deploy

--- src/main/java/brain/config/MvcConfig.java
+++ src/main/java/brain/config/MvcConfig.java
@@ -1,10 +1,7 @@
 package brain.config;
 
-import brain.service.UserService;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
