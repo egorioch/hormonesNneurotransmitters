5791b4218ddb094ca317b6a784f314168f801edc
SeeSoon21 <egornesobaka@yandex.ru>
2022-11-29 18:40:41 +0300

    useless

--- src/main/java/brain/Application.java
+++ src/main/java/brain/Application.java
@@ -3,6 +3,7 @@ package brain;
 import brain.service.MailSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,8 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"brain.repos"})
+@EntityScan("brain.domain")
+@ComponentScan(basePackages = {"brain.*"})
 public class Application {
 
     public static void main(String[] args) {
