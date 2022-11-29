d330a97ed85503d1e81b3193ea4be8efabd6eeee
SeeSoon21 <egornesobaka@yandex.ru>
2022-11-29 15:50:18 +0300

    heroku error

--- src/main/java/brain/Application.java
+++ src/main/java/brain/Application.java
@@ -2,12 +2,14 @@ package brain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"brain.service"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
